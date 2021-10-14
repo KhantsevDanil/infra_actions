91b638cd2c6697680dd0e4e5d5ff240764bc4415
KhantsevDanil <khantsev.danil@gmail.com>
2021-10-15 00:03:28 +0300

    python test 2

--- infra_project/infra_project/urls.py
+++ infra_project/infra_project/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('infra_app.urls', namespace='infra_app')),
+    path('second_page/', include('infra_app.urls', namespace='infra_app')),
     path('admin/', admin.site.urls),
 ]
